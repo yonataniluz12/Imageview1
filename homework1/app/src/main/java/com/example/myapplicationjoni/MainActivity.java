2fad3389bac3ddc6cfc72481b9581f1ea2778849
yonataniluz12 <95865008+yonataniluz12@users.noreply.github.com>
2022-02-02 09:41:16 +0200

    sher

---
+++ homework1/app/src/main/java/com/example/myapplicationjoni/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.myapplicationjoni;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    int x = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+    }
+
+    public void go(View view) {
+        x++;
+        btn.setText("This is click number: "+x);
+    }
+}
+
+
