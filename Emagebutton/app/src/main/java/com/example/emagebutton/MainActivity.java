e3cb65cc8d660f90c59f9955b9aa25e9b10f019f
yonataniluz12 <95865008+yonataniluz12@users.noreply.github.com>
2022-02-05 20:12:50 +0200

    Project approval

--- Emagebutton/app/src/main/java/com/example/emagebutton/MainActivity.java
+++ Emagebutton/app/src/main/java/com/example/emagebutton/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.emagebutton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView iv ;
+    ImageButton ib ;
+    int num ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iv = findViewById(R.id.iv);
+        ib = findViewById(R.id.ib);
+    }
+
+    public void go(View view) {
+        ib.setImageResource(R.drawable.click);
+        num = (int) (101 * Math.random())%3 + 1 ;
+        if (num == 1){
+            iv.setImageResource(R.drawable.ferrari);
+            ib.setImageResource(R.drawable.one);
+        }
+        else if (num == 12){
+            iv.setImageResource(R.drawable.lamborghini);
+            ib.setImageResource(R.drawable.two);
+        }
+        else {
+            iv.setImageResource(R.drawable.bugatti);
+            ib.setImageResource(R.drawable.three);
+        }
     }
 }
